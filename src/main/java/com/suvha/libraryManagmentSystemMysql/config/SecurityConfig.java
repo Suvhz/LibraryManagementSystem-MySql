4d1efbd8f18c0fbffa27fd8aac5acb9e2df46f5d
suvha <suvhashrestha66@gmail.com>
2018-10-31 10:30:58 +0545

    Added auth0 security config

---
+++ src/main/java/com/suvha/libraryManagmentSystemMysql/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.suvha.libraryManagmentSystemMysql.config;
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+ 	@Value(value = "https://suvha.com/api")
+    private String apiAudience;
+    @Value(value = "https://suvha.auth0.com/")
+    private String issuer;
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	 http.cors();
+        JwtWebSecurityConfigurer
+                .forRS256(apiAudience, issuer)
+                .configure(http)
+                .authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
